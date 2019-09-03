8acd26b9a295375da10f6ff20c8852ede12f0dc3
orionw <wellerorion@gmail.com>
2019-09-03 12:24:48 -0600

    init commit

---
+++ gru.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GRUModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
+        super().__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.gru = nn.GRU(embedding_dim, hidden_dim)
+        self.gru2out = nn.Linear(hidden_dim, vocab_size)
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+
+    def forward(self, inp, hidden):
+        # input dim                                             # batch_size
+        emb = self.embeddings(inp)                              # batch_size x embedding_dim
+        emb = emb.view(1, -1, self.embedding_dim)               # 1 x batch_size x embedding_dim
+        out, hidden = self.gru(emb, hidden)                     # 1 x batch_size x hidden_dim (out)
+        out = self.gru2out(out.view(-1, self.hidden_dim))       # batch_size x vocab_size
+        out = F.log_softmax(out, dim=1)
+        return out, hidden
